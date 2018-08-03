e0623b3f6842378898fde61573f9f6e30045c679
slavajamm <kvaracheliyaRAM@gmail.com>
2018-08-03 16:16:41 +0300

    Vers 1.0.0

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Calculator/>
+  </div>
+</template>
+
+<script>
+import Calculator from './components/Calculator'
+
+export default {
+  name: 'App',
+  components: {
+    Calculator
+  }
+}
+</script>
+
+<style>
+body{
+  text-align: center;
+}
+#app {
+  display: inline-block;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  width: 400px;
+  height: 500px;
+}
+</style>
